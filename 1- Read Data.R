7ec6909478580be7622da4effd063af2a2f70a87
Vaibhav Pitliya <34478349+VaibhavPitliya@users.noreply.github.com>
2018-02-04 01:19:24 +0530

    Add files via upload

---
+++ 1- Read Data.R
@@ -0,0 +1,12 @@
+###################################
+#  ### DESCRIBING DATA ###
+############
+
+#1) Setting working directory
+setwd("C:/Users/IIM/Google Drive/00 Data Analysis in Marketing/My R code and Datasets for DAM - final")
+
+#2) Read the data using read.csv
+store.df <- read.csv(paste("StoreData.csv", sep=""))
+#3) View the data frame in R
+View(store.df)
+############
